5d6b1a43da4eb04216a218214e38e37c774352d9
Stoica <petrutstoica123@yahoo.com>
2020-05-08 11:37:00 +0300

    Initial commit

---
+++ src/main/java/com/example/transvehobe/TransvehobeApplication.java
@@ -0,0 +1,13 @@
+package com.example.transvehobe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransvehobeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransvehobeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/transvehobe/TransvehobeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.transvehobe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransvehobeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

0348e2c1299b753e05694c18af489a8cd4c095b8
Stoica <petrutstoica123@yahoo.com>
2020-05-09 16:39:58 +0300

    Create Mock data

--- src/main/resources/db/migration/V1__Create_Tables.sql
+++ src/main/resources/db/migration/V1__Create_Tables.sql
@@ -4,5 +4,12 @@ create table users (
     password varchar(120),
     role varchar(255),
     username varchar(20) not null unique,
+    spoken_language varchar(255),
+    first_name varchar(12),
+    last_name varchar(120),
+    user_status varchar(255),
+    phone_number varchar(12),
+    driving_license_category varchar(255),
+
     primary key (id)
 )
